655f270d5ff944ab0b6dca57ec2122b1e9273b70
tuan3066 <132060398+tuan3066@users.noreply.github.com>
2024-05-25 16:29:41 +0700

    Update n.js

--- modules/commands/n.js
+++ modules/commands/n.js
@@ -1,152 +1,234 @@
 module.exports.config = {
-  name: "\n",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "đtai",
-  description: "cmm beo",
-  commandCategory: "Sex",
-  usages: "",
-  cooldowns: 5,
-  depndencies: {
-    "axios": ""
-  }
+	name: "*",
+	version: "1.0.5",
+	hasPermssion: 0,
+	credits: "Tpk",
+	description: "sailenh",
+	commandCategory: "Tiện ích",
+	usages: "Tiện ích",
+	cooldowns: 0
 };
 
-module.exports.run = async function ({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) {
-  const axios = require('axios');
+module.exports.run = async ({ api, event }) => {
+  const allicon = ["💞","💖","💗","💜","🌸","💗","💝","🎀","🌹","🍁","🎊","🌟","🍁"];
+const icon = allicon[Math.floor(Math.random()*allicon.length)];
+const axios = require('axios');
+    const request = require('request');
+    const fs = require("fs");
+    var os = require("os");
+	var cpus = os.cpus();
+	var chips;
+	for (var i of cpus) chips = i.model, speed = i.speed;
+	if (cpus == undefined);
+  const timeStart = Date.now();
+  /*const res = await axios.get(`https://tan.kizz-kinncuti.repl.co/images/cosplay`);
+var tpk = res.data.url;*/
   const moment = require("moment-timezone");
-  const apiUrl = "http://localhost:8300/haugai";
-  const dtaicutevc = ["1", "2", "3", "4", "6", "9", "12", "15", "18", "21", "24", "27"];
-  const array = [];
-
-  const randomIndex = Math.floor(Math.random() * dtaicutevc.length);
-  const nguyenductai = parseInt(dtaicutevc[randomIndex]);
-
-  for (let i = 0; i < nguyenductai; i++) {
-    const res = await axios.get(apiUrl);
-    const data = res.data.data;
-    const loadimg = (await axios.get(data, { responseType: 'stream' })).data;
-    array.push(loadimg);
-  }
-
-  var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || D/MM/YYYY");
-  var thu =
-    moment.tz('Asia/Ho_Chi_Minh').format('dddd');
-  const dateNow = Date.now();
+  const timeNow = moment.tz("Asia/Ho_Chi_Minh").format("DD/MM/YYYY || HH:mm:ss");
   const time = process.uptime(),
-    anh = Math.floor(time / (60 * 60)),
-    la = Math.floor((time % (60 * 60)) / 60),
-    dtai = Math.floor(time % 60);
-
-  const threadInfo = await api.getThreadInfo(event.threadID);
-  var { threadID, messageID, body } = event;
-  var name = await Users.getNameUser(event.senderID);
-  var tai = event.senderID;
-
-  api.sendMessage({
-    body: `Xin chào! Tôi là bot, dự án robot miễn phí của ALY Corporation. Bạn cần giúp gì?\n\nTime run - ${anh} : ${la} : ${dtai}`, attachment: array
-  }, event.threadID, (err, info) => {
+hours = Math.floor(time / (60 * 60)),
+minutes = Math.floor((time % (60 * 60)) / 60),
+seconds = Math.floor(time % 60);
+  const dateNow = Date.now();
+  const xuly = Math.floor((Date.now() - global.client.timeStart)/4444)
+ var trinhtrang = xuly < 10 ? "Tốt ✔️":
+  xuly > 10 && xuly < 100 ? "Ổn định 📊" : "deplay ";
+var tpk = [
+  "https://i.imgur.com/d3LPo9d.jpeg",
+"https://i.imgur.com/Ld414MY.jpeg",
+"https://i.imgur.com/FHGbBel.jpeg",
+"https://i.imgur.com/mxeIaMN.jpeg",
+"https://i.imgur.com/jJedCAy.jpeg",
+"https://i.imgur.com/qNU2aIw.jpeg",
+"https://i.imgur.com/y7xwPp5.jpeg",
+"https://i.imgur.com/SJFFVsn.jpeg",
+"https://i.imgur.com/kyoEe3K.jpeg",
+"https://i.imgur.com/4bBmlwm.jpeg",
+"https://i.imgur.com/bAnoZ6I.jpeg",
+"https://i.imgur.com/3iAQtgJ.jpeg",
+"https://i.imgur.com/eXzDbCq.jpeg",
+"https://i.imgur.com/njbOky4.jpeg",
+"https://i.imgur.com/bl5V9er.jpeg",
+"https://i.imgur.com/TS4eLOS.jpeg",
+"https://i.imgur.com/eYLcmYw.jpeg",
+"https://i.imgur.com/DGW5uy1.jpeg",
+"https://i.imgur.com/xLAReHI.jpeg",
+"https://i.imgur.com/hYUHZvd.jpeg",
+"https://i.imgur.com/Qh5RUE4.jpeg",
+"https://i.imgur.com/VlX2FFD.jpeg",
+"https://i.imgur.com/vKrJtox.jpeg",
+"https://i.imgur.com/I5rXAs1.jpeg",
+"https://i.imgur.com/s2VI3w9.jpeg",
+"https://i.imgur.com/hGrVKLA.jpeg",
+"https://i.imgur.com/8wHsFl2.jpeg",
+"https://i.imgur.com/i8HqieM.jpeg",
+"https://i.imgur.com/9L2HnXb.jpeg",
+"https://i.imgur.com/3UiRm0B.jpeg",
+"https://i.imgur.com/VevAPqv.jpeg",
+"https://i.imgur.com/pBMMx5R.jpeg",
+"https://i.imgur.com/6CX5nmV.jpeg",
+"https://i.imgur.com/fr9NXvz.jpeg",
+"https://i.imgur.com/4jceamg.jpeg",
+"https://i.imgur.com/yk8VGhl.jpeg",
+"https://i.imgur.com/xKDK4Hc.jpeg",
+"https://i.imgur.com/ep0zUWZ.jpeg",
+"https://i.imgur.com/6kQMw6a.jpeg",
+"https://i.imgur.com/Z1a1KxC.jpeg",
+"https://i.imgur.com/dX8KNAq.jpeg",
+"https://i.imgur.com/Zy2O584.jpeg",
+"https://i.imgur.com/hkoQ5Ue.jpeg",
+"https://i.imgur.com/iKtAAZ5.jpeg",
+"https://i.imgur.com/swIK791.jpeg",
+"https://i.imgur.com/etXCFVh.jpeg",
+"https://i.imgur.com/OirP1ZH.jpeg",
+"https://i.imgur.com/kfwBvUs.jpeg",
+"https://i.imgur.com/Ud9SbrN.jpeg",
+"https://i.imgur.com/ocgr6Ue.jpeg",
+"https://i.imgur.com/CpnTdVz.jpeg",
+"https://i.imgur.com/oGioFlL.jpeg",
+"https://i.imgur.com/0JtU1du.jpeg",
+"https://i.imgur.com/kP9NKyv.jpeg",
+"https://i.imgur.com/9qH7W9l.jpeg",
+"https://i.imgur.com/rn0HdXy.jpeg",
+"https://i.imgur.com/qdu2mvP.jpeg",
+"https://i.imgur.com/tBnVWr6.jpeg",
+"https://i.imgur.com/PFUFtsT.jpeg",
+"https://i.imgur.com/y1GI3nD.jpeg",
+"https://i.imgur.com/PrAPlM7.jpeg",
+"https://i.imgur.com/8JxaRwr.jpeg",
+"https://i.imgur.com/uIxci9E.jpeg",
+"https://i.imgur.com/SoicJLu.jpeg",
+"https://i.imgur.com/L25fv7L.jpeg",
+"https://i.imgur.com/Mwn8gAS.jpeg",
+"https://i.imgur.com/hrCtxMn.jpeg",
+"https://i.imgur.com/NCy70tv.jpeg",
+"https://i.imgur.com/wCUZRei.jpeg", "https://i.imgur.com/rmBmkQS.jpeg", "https://i.imgur.com/f3mZI5M.jpeg", "https://i.imgur.com/M9u5jnc.jpeg",
+"https://i.imgur.com/DVJkUZi.jpeg", "https://i.imgur.com/mqCcO7b.jpeg",
+"https://i.imgur.com/2k45Y0Q.jpeg",
+"https://i.imgur.com/uEWhCVu.jpeg",
+"https://i.imgur.com/TzTN7TF.jpeg", "https://i.imgur.com/PfRFeJZ.jpeg", "https://i.imgur.com/A8BnEdB.jpeg", "https://i.imgur.com/2VrAIYu.jpeg", "https://i.imgur.com/2V130U8.jpeg",
+"https://i.imgur.com/u8rpDrY.jpeg", "https://i.imgur.com/Q20VYTN.jpeg",
+"https://i.imgur.com/nTPUii6.jpeg",
+"https://i.imgur.com/aTdyuTz.jpeg",
+"https://i.imgur.com/JDmCudx.jpeg",
+"https://i.imgur.com/FMBbpR9.jpeg",
+"https://i.imgur.com/53KZb7V.jpeg",
+"https://i.imgur.com/5oOV4F7.jpeg", "https://i.imgur.com/gGUTX6S.jpeg", "https://i.imgur.com/d4nLOlm.jpeg",
+"https://i.imgur.com/jyrIzGI.jpeg",
+];
+var so = [
+  "2","3","4"
+];
+const tao = so[Math.floor(Math.random()*so.length)];
+  let image = [];
+ for(let i = 0; i < tao; i++) {
+   const a = tpk[Math.floor(Math.random()*tpk.length)];
+    const stream = (await axios.get(a, {
+        responseType: "stream"
+    })).data;
+    image.push(stream);
+};
+  const msg = {
+    body: `💓 ==== [ 𝗭𝗜𝗢𝗘𝗦 ] ==== 💓\n━━━━━━━━━━━━━━━━━━\n⏰ 𝗧𝗶𝗺𝗲: ${timeNow}\n⏳ 𝗧𝗵𝗼̛̀𝗶 𝗴𝗶𝗮𝗻 𝗼𝗻𝗹: ${hours}:${minutes}:${seconds}\n⚙️ 𝗧𝗶̀𝗻𝗵 𝘁𝗿𝗮̣𝗻𝗴: ${trinhtrang}\n🌸 𝗧𝗼̂́𝗰 đ𝗼̣̂ 𝘅𝘂̛̉ 𝗹𝘆́: ${xuly} 𝗴𝗶𝗮̂𝘆\n⏳ Đ𝗼̣̂ 𝘁𝗿𝗲̂̃: ${Date.now() - timeStart}ms
+━━━━━━━━━━━━━━━━━━━━━━
+👍 𝗫𝗲𝗺 𝗻𝗵𝘂̛̃𝗻𝗴 𝗹𝗲̣̂𝗻𝗵 𝘁𝗵𝘂̛𝗼̛̀𝗻𝗴 𝗱𝘂̀𝗻𝗴
+🥰 𝗫𝗲𝗺 𝘁𝗵𝗼̂𝗻𝗴 𝘁𝗶𝗻 𝗮𝗱𝗺𝗶𝗻
+😆 𝗖𝗮́𝗰𝗵 𝗱𝘂̀𝗻𝗴 𝗯𝗼𝘁 𝗰𝗼̛ 𝗯𝗮̉𝗻
+💵 𝗖𝗵𝗶 𝘁𝗶𝗲̂́𝘁 𝗰𝗮́𝗰𝗵 𝘁𝗵𝘂𝗲̂ 𝗯𝗼𝘁
+😍 𝗧𝗵𝗼̂𝗻𝗴 𝘁𝗶𝗻 𝘃𝗲̂̀ 𝗳𝗶𝗹𝗲 𝗯𝗼𝘁
+━━━━━━━━━━━━━━━━━━━━━━
+👑 𝗧𝗵𝗮̉ 𝗰𝗮̉𝗺 𝘅𝘂́𝗰 𝘃𝗮̀𝗼 𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝗻𝗮̀𝘆 𝘃𝗼̛́𝗶 𝗶𝗰𝗼𝗻 𝗯𝗮̣𝗻 𝗺𝘂𝗼̂́𝗻 𝘅𝗲𝗺`,
+    attachment: image
+};
+    api.sendMessage(msg,event.threadID, (err, info) => {
     global.client.handleReaction.push({
-      name: this.config.name,
+      name: this.config.name, 
       messageID: info.messageID,
       author: event.senderID,
     })
-  }, event.messageID);
+    },event.messageID);
+     }
+module.exports.handleReaction = async ({ event, api, handleReaction, Currencies, Users}) => {
+const axios = global.nodemodule["axios"];
+const fs = global.nodemodule["fs-extra"];
+const { threadID,reaction,messageID, userID } = event;
+if (reaction == '👍') {
+  api.unsendMessage(handleReaction.messageID);
+var msg = `📗 ==== 𝗟𝗜𝗦𝗧 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ====📗 
+━━━━━━━━━━━━━━━━━━━━━
+𝟭. !𝗵𝗲𝗹𝗽 𝗮𝗹𝗹 𝗵𝗼𝗮̣̆𝗰 𝘁𝗲̂𝗻 𝗹𝗲̣̂𝗻𝗵
+𝟮. 𝗱𝘂̀𝗻𝗴 !𝗵𝗲𝗹𝗽: 𝗻𝗲̂́𝘂 𝗺𝘂𝗼̂́𝗻 𝘅𝗲𝗺 𝘁𝗮̂́𝘁 𝗰𝗮̉ 𝗹𝗲̣̂𝗻𝗵 𝗯𝗼𝘁 𝗰𝗼́
+𝟯. 𝗱𝘂̀𝗻𝗴 !𝗰𝗵𝗲𝗰𝗸𝘁𝘁: đ𝗲̂̉ 𝘅𝗲𝗺 𝘀𝗼̂́ 𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝗺𝗮̀ 𝗯𝗮̣𝗻 đ𝗮̃ 𝗻𝗵𝗮̆́𝗻
+𝟰. 𝗱𝘂̀𝗻𝗴 !𝗰𝗵𝗲𝗰𝗸: 𝗻𝗲̂́𝘂 𝗺𝘂𝗼̂́𝗻 𝘅𝗲𝗺 𝗰𝗮́𝗰 𝗹𝗲̣̂𝗻𝗵 𝘃𝗲̂̀ 𝗰𝗵𝗲𝗰𝗸
+𝟱. !𝗯𝗼𝘅 𝗶𝗻𝗳𝗼: đ𝗲̂̉ 𝘅𝗲𝗺 𝘁𝗵𝗼̂𝗻𝗴 𝘁𝗶𝗻 𝘃𝗲̂̀ 𝗯𝗼𝘅 
+𝟲. !𝗴𝗵𝗲𝗽: 𝗽𝗵𝗶𝗲̂𝗻 𝗯𝗮̉𝗻 𝗰𝗮𝗻𝘃𝗮𝘀 
+𝟳. !𝗴𝗵𝗲́𝗽: 𝗰𝘂̃𝗻𝗴 𝗹𝗮̀ 𝗴𝗵𝗲́𝗽 𝗻𝗵𝘂̛̃𝗻𝗴 𝗹𝗮̀ 𝗽𝗵𝗶𝗲̂𝗻 𝗯𝗮̉𝗻 𝘁𝗶𝗻𝗱𝗲𝗿
+𝟴. !𝗹𝗼𝗰𝗺𝗲𝗺: 𝗹𝗼̣𝗰 𝗻𝗵𝘂̛̃𝗻𝗴 𝘁𝗵𝗮̀𝗻𝗵 𝘃𝗶𝗲̂𝗻 𝗸𝗵𝗼̂𝗻𝗴 𝘁𝘂̛𝗼̛𝗻𝗴 𝘁𝗮́𝗰
+𝟵. !𝘀𝗲𝘁𝗻𝗮𝗺𝗲 + 𝘁𝗲̂𝗻: 𝘀𝗲𝘁 𝗯𝗶𝗲̣̂𝘁 𝗱𝗮𝗻𝗵 𝗰𝘂̉𝗮 𝗯𝗮̣𝗻 𝗼̛̉ 𝗻𝗵𝗼́𝗺
+𝟭𝟬. !𝘆𝗼𝘂𝘁𝘂𝗯𝗲 & 𝘁𝗲̂𝗻 𝗰𝗹𝗶𝗽: 𝘁𝗮̉𝗶 𝗰𝗹𝗶𝗽 𝘁𝗿𝗲̂𝗻 𝘆𝘁𝗯
+𝟭𝟭. !𝘁𝗶𝗸𝘁𝗼𝗸: 𝘁𝗮̉𝗶 𝘃𝗶𝗱𝗲𝗼 𝘁𝗶𝗸𝘁𝗼𝗸 𝗱𝘂̀𝗻𝗴 𝗹𝗲̣̂𝗻𝗵 𝗱̶𝗲̂̉ 𝗯𝗶𝗲̂́𝘁 𝗰𝗵𝗶 𝘁𝗶𝗲̂́𝘁
+𝟭𝟮. !𝘀𝗶𝗻𝗴 𝗼𝗿 𝘁𝗲̂𝗻 𝗯𝗮̀𝗶 𝗵𝗮́𝘁: 𝗽𝗵𝗮́𝘁 𝗯𝗮̀𝗶 𝗵𝗮́𝘁 𝗱𝗮̣𝗻𝗴 𝘃𝗼𝗶𝗰𝗲𝘀
+𝟭𝟯. !𝗶𝗻𝗳𝗼: 𝘅𝗲𝗺 𝘁𝗵𝗼̂𝗻𝗴 𝘁𝗶𝗻 𝘃𝗲̂̀ 𝗮𝗰𝗰 𝗰𝘂̉𝗮 𝗯𝗮̣𝗻
+𝟭𝟰. !𝗶𝗺𝗴𝘂𝗿 + 𝗿𝗲𝗽𝗹𝘆 𝗮̉𝗻𝗵: 𝗹𝗮̂́𝘆 𝗹𝗶𝗻𝗸 𝗶𝗺𝗴𝘂𝗿
+𝟭𝟱. !𝗻𝗲𝘁𝗮𝗻𝗵 & 𝗿𝗲𝗽𝗹𝘆 𝗮̉𝗻𝗵: 𝗹𝗮̀𝗺 𝗻𝗲́𝘁 𝗮̉𝗻𝗵 𝗺𝗮̀ 𝗯𝗮̣𝗻 𝗿𝗲𝗽𝗹𝘆
+𝟭𝟲. !𝗮𝘃𝘁𝗱𝗼𝗶: 𝗴𝘂̛̉𝗶 𝗮𝘃𝘁𝗱𝗼𝗶 𝘁𝗵𝗲𝗼 𝗯𝗮̣𝗻 𝗰𝗵𝗼̣𝗻
+𝟭𝟳. 𝗾𝗿 + 𝗾𝗿𝘀: 𝗹𝗮̀𝗺 𝗾𝗿 𝘃𝗮̀ 𝗾𝘂𝗲́𝘁 𝗾𝗿
+
+🌸 𝗣𝗵𝗶́𝗮 𝘁𝗿𝗲̂𝗻  𝗗𝘂𝗻𝗴 𝗻𝗵𝘂̛̃𝗻𝗴 𝗹𝗲̣̂𝗻𝗵 𝘁𝗵𝘂̛𝗼̛̀𝗻𝗴 đ𝘂̛𝗼̛̣𝗰 𝘀𝘂̛̉ 𝗱𝘂̣𝗻𝗴
+`
+        return api.sendMessage({body: msg, attachment: (await global.nodemodule["axios"]({
+url: (await global.nodemodule["axios"]('https://apivip.nvt20011.repl.co/image/loli')).data.url,
+method: "GET",
+responseType: "stream"
+})).data
+},event.threadID);
 }
-module.exports.handleReaction = async ({ event, api, handleReaction, Currencies, Users }) => {
-  const axios = require('axios');
-  const request = require("request");
-  const fs = require("fs-extra");
-  const { threadID, messageID, userID } = event;
-  const moment = require("moment-timezone");
-  var timeNow = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
-  async function streamURL(url, mime = 'jpg') {
-    const dest = `${__dirname}/cache/${Date.now()}.${mime}`,
-      downloader = require('image-downloader'),
-      fse = require('fs-extra');
-    await downloader.image({
-      url, dest
-    });
-    setTimeout(j => fse.unlinkSync(j), 60 * 1000, dest);
-    return fse.createReadStream(dest);
-  };
+else if (reaction == '🥰') {
+  api.unsendMessage(handleReaction.messageID);
+    return api.sendMessage(`👑 === 𝗧𝗛𝗢̂𝗡𝗚 𝗧𝗜𝗡 𝗔𝗗𝗠𝗜𝗡 ===👑
+━━━━━━━━━━━━━━━━━━━━
+👤 𝗧𝗲̂𝗻: Doan Thanh Tuan
+🔰 𝗕𝗶𝗲̣̂𝘁 𝗱𝗮𝗻𝗵:  𝗞mZioes
+🦁 𝗖𝘂𝗻𝗴: 𝗕𝗮̉𝗼 𝗯𝗶̀𝗻𝗵
+👶 𝗡𝗴𝗮̀𝘆 𝘀𝗶𝗻𝗵: 13/12/2009
+🗾 𝗡𝗼̛𝗶 𝗼̛̉: Hà Nội 
+☎️ 𝗟𝗶𝗲̂𝗻 𝗵𝗲̣̂: https://www.facebook.com/profile.php?id=tuandzas
 
-  let threadInfo = await api.getThreadInfo(event.threadID);
-  let threadName = threadInfo.threadName;
-  let id = threadInfo.threadID;
-  let sex = threadInfo.approvalMode;
-  var pd = sex == false ? 'Tắt' : sex == true ? 'Bật' : '\n';
-  let color = threadInfo.color;
-  let icon = threadInfo.emoji;
-  let threadMem = threadInfo.participantIDs.length;
-  if (event.userID != handleReaction.author) return;
-  if (event.reaction != "👍") return;
-  api.sendMessage({
-    body: `=====「 𝗧𝗛𝗢̂𝗡𝗚 𝗧𝗜𝗡 𝗡𝗛𝗢́𝗠 」=====\n\n🏘️ 𝗧𝗲̂𝗻 𝗻𝗵𝗼́𝗺: ${threadName}\n⚙️ 𝗜𝗗 𝗻𝗵𝗼́𝗺: ${id}\n👥 𝗦𝗼̂́ 𝘁𝗵𝗮̀𝗻𝗵 𝘃𝗶𝗲̂𝗻 𝗻𝗵𝗼́𝗺: ${threadMem}\n🌷 𝗣𝗵𝗲̂ 𝗱𝘂𝘆𝗲̣̂𝘁: ${pd}\n😻 𝗕𝗶𝗲̂̉𝘂 𝘁𝘂̛𝗼̛̣𝗻𝗴 𝗰𝗮̉𝗺 𝘅𝘂́𝗰: ${icon ? icon : 'Không sử dụng'}\n💝 𝗠𝗮̃ 𝗴𝗶𝗮𝗼 𝗱𝗶𝗲̣̂𝗻: ${color}\n━━━━━━━━━━━━━━━━━━\n🍑 𝗧𝗼̂̉𝗻𝗴 𝘀𝗼̂́ 𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝗰𝘂̉𝗮 𝗻𝗵𝗼́𝗺: ${threadInfo.messageCount}\n🎀 𝟭: 𝗔𝗱𝗺𝗶𝗻 𝗯𝗼𝘁\n📑 𝟮: 𝗤𝘂𝗮̉𝗻 𝘁𝗿𝗶̣ 𝘃𝗶𝗲̂𝗻 𝗻𝗵𝗼́𝗺\n📝𝗥𝗲𝗽𝗹𝘆 𝘁𝗵𝗲𝗼 𝘀𝗼̂́ 𝘅𝗲𝗺 𝗗𝗮𝗻𝗵 𝘀𝗮́𝗰𝗵 𝗤𝗨𝗔̉𝗡 𝗧𝗥𝗜̣ 𝗩𝗜𝗘̂𝗡 𝗻𝗵𝗼́𝗺 𝘃𝗮̀ 𝗔𝗗𝗠𝗜𝗡 𝗕𝗢𝗧 `,
-    attachment: await streamURL(threadInfo.imageSrc)
-  }, event.threadID, (err, info) => {
-
-    global.client.handleReply.push({
-      type: "choosee",
-      name: this.config.name,
-      author: event.senderID,
-      messageID: info.messageID
-    })
-  })
-}
-module.exports.handleReply = async function ({
-  args,
-  event,
-  Users,
-  api,
-  handleReply,
-  Currencies,
-  permssion,
-  getText,
-  __GLOBAL
-}) {
-  const axios = require("axios");
-  const fs = require("fs-extra");
-  api.sendMessage(`vui lòng chờ 1 xíu`, event.threadID, (err, info) =>
-    setTimeout(() => { api.unsendMessage(info.messageID) }, 100000));
-  const request = require("request");
-  const nameUser = (await Users.getData(event.senderID)).name || (await Users.getInfo(envent.senderID)).name;
-  let data = (await Currencies.getData(event.senderID)).ghepTime;
-
-
-  switch (handleReply.type) {
-    case "choosee": {
-      switch (event.body) {
-        case "":
-        case "":
-        case "1": {
-          const axios = require('axios');
-          const request = require("request");
-          const fs = require("fs-extra");
-          const name = (await Users.getData(idAdmin)).name
-
-          api.sendMessage(`𝗗𝗮𝗻𝗵 𝘀𝗮́𝗰𝗵 𝗔𝗗𝗠𝗜𝗡 𝗕𝗢𝗧:\n${name}`, event.threadID, event.messageID)
-        }
-
-        case "2": {
-          const axios = require('axios');
-          const request = require("request");
-          const fs = require("fs-extra");
-          var listad_msg = '';
-          var adminIDs = threadInfo.adminIDs;
-          for (let get of adminIDs) {
-            const infoUsers = await Users.getInfo(get.id);
-            listad_msg += `• ${infoUsers.name}\n`
-          }
-          api.unsendMessage(handleReply.messageID);
-          return api.sendMessage({ body: `𝗗𝗮𝗻𝗵 𝘀𝗮́𝗰𝗵 𝗤𝗨𝗔̉𝗡 𝗧𝗥𝗜̣ 𝗩𝗜𝗘̂𝗡\n${qtv}`, attachment }, event.threadID, event.messageID)
-        }
-          break;
-        default:
-          const choose = parseInt(event.body);
-          if (isNaN(event.body)) return api.sendMessage("→ 𝐕𝐮𝐢 𝐥𝐨̀𝐧𝐠 𝐧𝐡𝐚̣̂𝐩 𝟏 𝐜𝐨𝐧 𝐬𝐨̂́", event.threadID, event.messageID);
-          if (choose > 2 || choose < 1) return api.sendMessage("→ 𝐋𝐮̛̣𝐚 𝐜𝐡𝐨̣𝐧 𝐤𝐡𝐨̂𝐧𝐠 𝐧𝐚̆̀𝐦 𝐭𝐫𝐨𝐧𝐠 𝐝𝐚𝐧𝐡 𝐬𝐚́𝐜𝐡.", event.threadID, event.messageID);
-      }
+🌸 𝗣𝗵𝗮́𝗶 𝘁𝗿𝗲̂𝗻 𝗹𝗮̀ 𝘁𝗵𝗼̂𝗻𝗴 𝘁𝗶𝗻 𝗮𝗱𝗺𝗶𝗻 𝗰𝗵𝗶́𝗻𝗵`, event.threadID, event.messageID);
     }
+  else if (reaction == '😆') {
+    api.unsendMessage(handleReaction.messageID);
+    return api.sendMessage(`❤️ =『 𝗖𝗔́𝗖𝗛 𝗗𝗨̀𝗡𝗚 𝗖𝗢̛ 𝗕𝗔̉𝗡 』=❤️
+
+𝗛𝘂̛𝗼̛́𝗻𝗴 𝗱𝗮̂̃𝗻 𝘀𝘂̛̉ 𝗱𝘂̣𝗻𝗴 𝗰𝗼̛ 𝗯𝗮̉𝗻!
+
+😻 𝗡𝗵𝗮̣̂𝗽 !𝗺𝗲𝗻𝘂 𝗵𝗼𝗮̣̆𝗰 !𝗵𝗲𝗹𝗽 đ𝗲̂̉ 𝘅𝗲𝗺 𝘁𝗮̂́𝘁 𝗰𝗮̉ 𝗹𝗲̣̂𝗻𝗵 𝘁𝗿𝗲̂𝗻 𝗕𝗼𝘁
+🎊 𝗡𝗵𝗮̣̂𝗽 !𝗿𝘂𝗹𝗲 đ𝗲̂̉ 𝘅𝗲𝗺 𝗹𝘂𝗮̣̂𝘁 𝗻𝗵𝗼́𝗺
+𝗻𝗲̂́𝘂 𝗰𝗵𝘂̛𝗮 𝗰𝗼́ 𝗵𝗮̃𝘆 𝘁𝗵𝗲̂𝗺 𝗯𝗮̆̀𝗻𝗴 𝗰𝗮́𝗰𝗵 
+🍏 !𝗿𝘂𝗹𝗲 𝗮𝗱𝗱 + [𝗟𝘂𝗮̣̂𝘁 𝗡𝗵𝗼́𝗺] đ𝗲̂̉ 𝘁𝗵𝗲̂𝗺 𝗹𝘂𝗮̣̂𝘁
+🌸 !𝗗𝘂̀𝗻𝗴 𝗹𝗲̣̂𝗻𝗵 !𝗰𝗮𝗹𝗹𝗮𝗱 + [𝗟𝗼̛̀𝗶 𝗡𝗵𝗮̆́𝗻] 
+đ𝗲̂̉ 𝗰𝗵𝘂𝘆𝗲̂̉𝗻 𝗹𝗼̛̀𝗶 đ𝗲̂́𝗻 𝗮𝗱𝗺𝗶𝗻
+
+𝗖𝗼́ 𝘁𝗵𝗮̆́𝗰 𝗺𝗮̆́𝗰 𝗵𝗮𝘆 𝘃𝗮̂́𝗻 đ𝗲̂̀ 𝗴𝗶̀ 𝘁𝗵𝗶̀ 𝗹𝗶𝗲̂𝗻 
+☎️ 𝗵𝗲̣̂ 𝗾𝘂𝗮 𝗳𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
+https://www.facebook.com/profile.php?id=tuandzas `, event.threadID, event.messageID);
   }
+else if (reaction == '💵') {
+  api.unsendMessage(handleReaction.messageID);
+    return api.sendMessage(`🔔 =====『 𝗧𝗛𝗨𝗘̂ 𝗕𝗢𝗧 』=====🔔
+━━━━━━━━━━━━━━━━━━━━━
+💵 𝗖𝗮́𝗰𝗵 𝘁𝗵𝘂̛́𝗰 𝘁𝗵𝘂𝗲̂ 𝗯𝗼𝘁
+🌸 𝗧𝗵𝘂𝗲̂ 𝗯𝗼𝘁 50k/1tháng
+👑 𝗡𝗴𝘂̛𝗼̛̀𝗶 𝘁𝗵𝘂𝗲̂ đ𝘂̛𝗼̛̣𝗰 𝘁𝗵𝗲̂𝗺 𝗹𝗮̀𝗺 𝗮𝗱𝗺𝗶𝗻 𝗯𝗼𝘁
+☎️ 𝗟𝗶𝗲̂𝗻 𝗵𝗲̣̂: https://www.facebook.com/tony.derby.773
+`, event.threadID, event.messageID);
 }
+  else if (reaction == '😍') {
+    api.unsendMessage(handleReaction.messageID);
+    return api.sendMessage(`
+🎊 Đ𝗮̂𝘆 𝗹𝗮̀ 𝗠𝗶𝗿𝗮𝗶 𝗯𝗼𝘁 𝗭𝗜𝗢𝗘𝗦💍𝗧𝘂𝗮𝗻 đ𝘂̛𝗼̛̣𝗰 𝗮𝗱𝗺𝗶𝗻 𝗹𝗮̂́𝘆 𝘁𝘂̛̀ 𝘀𝗿𝗰 𝗱 - 𝗷𝘂𝗸𝗲 𝗰𝘂̉𝗮 𝗱𝗶𝗲̣̂𝗻 𝘃𝗲̂̀ 𝘃𝗮̀ 𝗺𝗼𝗱 𝗹𝗮̣𝗶 đ𝗲̂̉ 𝘁𝗶𝗲̂́𝗽 𝘁𝘂̣𝗰 𝗽𝗵𝗮́𝘁 𝘁𝗿𝗶𝗲̂̉𝗻
+𝗠𝗼̣𝗶 𝘁𝗵𝗮̆́𝗰 𝗺𝗮̆́𝗰 𝗰𝗼́ 𝘁𝗵𝗲̂̉ 𝗹𝗶𝗲̂𝗻 𝗵𝗲̣̂ 𝗮𝗱𝗺𝗶𝗻
+🌐 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: https://www.facebook.com/tony.derby.773
+`, event.threadID, event.messageID);
+  }
+    };
